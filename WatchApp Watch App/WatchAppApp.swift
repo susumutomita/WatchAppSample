fd2d3cdd29dc3e675da8eda6864d050781a4e220
susumu tomita <oyster880@gmail.com>
2022-10-23 07:58:51 +0900

    Initial Commit

---
+++ WatchApp Watch App/WatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAppApp.swift
+//  WatchApp Watch App
+//
+//  Created by susumu tomita on 2022/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
